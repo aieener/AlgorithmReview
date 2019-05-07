80596ca1ac051bfb1932237aec8c10b2dd35f2eb
Yuding Ai <yudingai@seas.upenn.edu>
2019-05-06 21:18:42 -0400

    May 6 review String I

--- src/Class_07_String_HashTable/Strstr.java
+++ src/Class_07_String_HashTable/Strstr.java
@@ -1,85 +1,35 @@
 package Class_07_String_HashTable;
 
 public class Strstr {
-    /**
-     * My Sol
-     * Naive
-     */
-    public int strstr(String large, String small) {
-        if(small.length() == 0) {
-            return 0;
-        }
-        int len = large.length() - small.length();
+	public int strstrI(String large, String small) {
+		if (large.length() < small.length()) {
+			return -1;
+		}
+		if (small.length() == 0) {
+			return 0;
+		}
+		for (int i = 0; i <= large.length() - small.length(); i++) {
+			if (equals(large, i, small)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-        for(int i = 0; i <= len ; i++) {
-            for(int j = i; j < small.length() + i; j++) {
-                if(large.charAt(j) != small.charAt(j - i)){
-                    break;
-                }
-                if(j == small.length() + i - 1){
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
+	public boolean equals(String large, int start, String small) {
+		for (int i = 0; i < small.length(); i++) {
+			if (large.charAt(i + start) != small.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    /**
-     * LaiOffer Naive Sol
-     */
-    public int strstrI(String large, String small) {
-        if(large.length() < small.length()) {
-            return -1;
-        }
-        if(small.length() == 0) {
-            return 0;
-        }
-        for(int i = 0; i <= large.length() - small.length(); i++) {
-            if(equals(large, i, small)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public boolean equals(String large, int start, String small) {
-        for(int i = 0; i < small.length(); i++) {
-            if(large.charAt(i + start) != small.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        String input = "bcabc";
-        String small = "ab";
-        Strstr str = new Strstr();
-        int res = str.strstr(input,small);
-        System.out.println(res);
-    }
-
-    //--- prac ---
-    public int strstr2 (String large, String small) {
-        if(large.length() < small.length()){
-            return -1;
-        }
-        if(small.length() == 0) {
-            return 0;
-        }
-        for(int i = 0; i <= large.length() - small.length(); i++) {
-            if(equals2(large, i, small)){
-                return i;
-            }
-        }
-        return -1;
-    }
-    private boolean equals2(String large, int start, String small){
-        for(int i = 0; i < small.length(); i++) {
-            if(large.charAt(start + i) != small.charAt(i)){
-                return false;
-            }
-        }
-        return true;
-    }
+	public static void main(String[] args) {
+		String input = "bcabc";
+		String small = "ab";
+		Strstr str = new Strstr();
+		int res = str.strstr(input, small);
+		System.out.println(res);
+	}
 }
--- src/Class_07_String_HashTable/deDupIV.java
+++ src/Class_07_String_HashTable/deDupIV.java
@@ -2,95 +2,45 @@ package Class_07_String_HashTable;
 
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.concurrent.DelayQueue;
 
-public class deDupIV {
-    /**
-     * My Sol with LaiOffer method
-     * Using a stack
-     */
-    public String deDup(String input) {
-        if(input == null || input.length() == 0) {
-            return input;
-        }
-        char[] array = input.toCharArray();
-        Deque<Character> stack = new LinkedList<>();
-        int i = 0;
-        stack.addFirst(array[i]);
-        i++;
+public class DeDupIV {
+	/**
+	 * abbbaaccz f |z
+	 *     f
+	 */
+	public String deDup(String input) {
+		if (input == null || input.length() == 0) {
+			return input;
+		}
+		Deque<Character> stack = new LinkedList<>();
+		char[] inputArr = input.toCharArray();
+		stack.offerFirst(inputArr[0]);
+		for (int i = 1; i < inputArr.length; i++) {
+			if (stack.isEmpty() || stack.peekFirst() != inputArr[i]) {
+				stack.offerFirst(inputArr[i]);
+			} else {
+				while (i < inputArr.length && stack.peekFirst() == inputArr[i]) {
+					i++;
+				}
+				stack.pollFirst();
+				i--; // this is important!
+			}
+		}
+		return extractStringFromStack(stack);
+	}
 
-        while(i != array.length) {
-            if(stack.isEmpty() || stack.peekFirst() != array[i]){
-                stack.addFirst(array[i]);
-                i++;
-            } else {
-                while(i != array.length &&
-                        stack.peekFirst() == array[i]){
-                    i++;
-                }
-                stack.pollFirst();
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()){
-            sb.insert(0,stack.pollFirst());
-//            sb.append(stack.pollFirst());
-        }
-//        sb.reverse();
-        return sb.toString();
-    }
+	private String extractStringFromStack(Deque<Character> stack) {
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty()) {
+			sb.append(stack.pollLast());
+		}
+		return sb.toString();
+	}
 
-    /**
-     * LaiOffer Sol
-     */
-    public String deDup2(String input) {
-        if(input == null || input.length() == 0) {
-            return input;
-        }
-
-        char[] array = input.toCharArray();
-        int end = 0;
-        for(int i = 1; i < array.length; i++) {
-            if(end == -1 || array[i] != array[end]) {
-                array[++end] = array[i];
-            } else {
-                end--;
-                while(i + 1 < array.length && array[i] == array[i + 1]){
-                    i++;
-                }
-            }
-        }
-        return new String(array, 0, end + 1);
-    }
-
-    public static void main(String[] args) {
-        deDupIV dd = new deDupIV();
-        String input = "abbbaaccz";
-        String res = dd.deDup3(input);
-        System.out.println(res);
-    }
-
-    // --- prac ---
-    // 2 ptr version
-    public String deDup3(String input) {
-        if(input == null || input.length() == 0) {
-            return input;
-        }
-        char[] array = input.toCharArray();
-        int f = 1;
-        int s = 0;
-        while( f < array.length) {
-            if(s == -1 || array[s] != array[f]){
-                s++;
-                array[s] =array[f];
-            } else {
-                s--;
-                while(f + 1 < array.length && array[f] == array[f + 1]){
-                    f++;
-                }
-            }
-            f++;
-        }
-        return new String (array, 0, s + 1);
-    }
+	public static void main(String[] args) {
+		DeDupIV dd = new DeDupIV();
+		String input = "abbbaaccz";
+		String res = dd.deDup(input);
+		System.out.println(res);
+	}
 }
